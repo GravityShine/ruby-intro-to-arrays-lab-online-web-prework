eb6d363841d41b2d120825998f5f1a2b27be4efa
Samuel Kovacs <samuelbkovacs@gmail.com>
2019-03-23 07:19:39 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -7,7 +7,6 @@ def array_with_two_elements
 end
 
 
-def first_element
-taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
-taylor_swift[0]
+def first_element(array)
+  array[0]
 end
