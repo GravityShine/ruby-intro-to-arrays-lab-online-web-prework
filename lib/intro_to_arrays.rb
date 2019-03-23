fccf2e077676484f28df90a82d07c54c44ce8f7e
Samuel Kovacs <samuelbkovacs@gmail.com>
2019-03-23 07:22:07 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -8,9 +8,11 @@ end
 
 
 def first_element(array)
+  array = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
   array[0]
 end
 
-def third_element ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+def third_element(array)
+  array = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
   array[2]
 end
